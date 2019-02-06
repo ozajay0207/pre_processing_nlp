d54099ea1f533c5cac6061f251f87f981f54da1c
Jay Oza <ozajay0207@gmail.com>
2019-02-06 21:40:55 +0530

    own tokenizer partial

--- word_tokenize_nltk.py
+++ word_tokenize_nltk.py
@@ -1,12 +1,12 @@
 from nltk.tokenize import sent_tokenize,word_tokenize
 import nltk
 
-f = open("xad", "r")
-f1 = open("word_tokenize.txt", "a")
+f = open("demo1.txt", "r")
+f1 = open("word_tokenize_nltk.txt", "a")
 
 for i in word_tokenize(f.read()):
 	f1.write(i)
-	f1.write(" ")
+	f1.write('\n')
 
 
 
